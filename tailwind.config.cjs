eda2e18a530a3f096c3f8d9f66c0ec04f45a3bae
Farrukh Ayaz <farrukh.ayazz3@gmail.com>
2023-03-28 00:17:00 +0500

    public/index.html added to tailwind config for build

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,tsx}"],
+  purge: [
+    "./src/components/**/*.{js,tsx}",
+    "./public/index.html",
+  ],
   theme: {
     extend: {},
   },
