4de4b5dbc33138de174b1278bc01edbb55768a96
Farrukh Ayaz <farrukh.ayazz3@gmail.com>
2023-03-28 00:47:09 +0500

    tailwind plugin added to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import windiCSS from 'vite-plugin-windicss'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), windiCSS()],
 })
