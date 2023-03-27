3732341f820536202d3c77cd7d4f1d7498b8a781
Farrukh Ayaz <farrukh.ayazz3@gmail.com>
2023-03-28 00:28:08 +0500

    Prior to this change, This change

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx}", "./dist/index.html"],
+  content: ["./src/**/*.{html,js,tsx}"],
   purge: [
-    './public/**/*.html'
+    './dist/**/*.html'
   ],
   theme: {
     extend: {},
