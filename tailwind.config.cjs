f08af520ffab3312802ba3616c7ab6489fbc268a
Farrukh Ayaz <farrukh.ayazz3@gmail.com>
2023-03-28 00:25:55 +0500

    .

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,tsx}", "./dist/index.html",],
+  content: ["./src/**/*.{html,js,tsx}", "./dist/index.html"],
+  purge: [
+    './public/**/*.html'
+  ],
   theme: {
     extend: {},
   },
